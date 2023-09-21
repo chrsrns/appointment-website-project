99ef53e7c9cebe589631183e125950d32ba1e1b9
Christian Aranas <aranaschristianlouise@gmail.com>
2023-09-21 00:28:31 -0400

    More UI implementations
    Added Dashboard code
    Added initial routing via React Router
    Added and implemented SASS as a dependency
    Minor refactoring in `App.tsx`

--- src/frontend-react/src/App.tsx
+++ src/frontend-react/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./App.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   Button,
   Card,
@@ -15,11 +15,14 @@ import {
   ListGroup,
   ListGroupItem,
   Table,
+  ButtonGroup,
+  ToggleButton,
 } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const TopBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary shadow-sm">
       <Container>
         <Navbar.Brand href="#home">Integrated School</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -51,14 +54,203 @@ type JumbotronProps = {
 };
 const Jumbotron: React.FC<JumbotronProps> = ({ header, children }) => {
   return (
-    <div className="jumbotron">
+    <div className="jumbotron shadow-sm">
       <h1>{header}</h1>
       <p>{children}</p>
     </div>
   );
 };
 
-const App = () => {
+const Dashboard = ({ sidebarbtn_onClick }) => {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/users/announcements")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <p className="float-start d-lg-none d-md-block">
+        <Button variant="primary" size="sm" onClick={sidebarbtn_onClick}>
+          <i className="bi bi-chevron-right"></i>
+        </Button>
+      </p>
+      <Jumbotron header="Dashboard">
+        Browse a summary of announcements and appointments relevant to you.
+      </Jumbotron>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header as="h2">Announcements</Card.Header>
+            <Card.Body>
+              <ListGroup>
+                {data.map((announcement) => {
+                  return <ListGroupItem>{announcement.content}</ListGroupItem>;
+                })}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card className="shadow-sm">
+            <Card.Header as={"h2"}>My Appointments</Card.Header>
+            <Card.Body>
+              <div
+                className="table-responsive"
+                style={{
+                  height: "25rem",
+                  position: "relative",
+                }}
+              >
+                <Table className="appointments-table">
+                  <thead>
+                    <tr className="text-uppercase">
+                      <th scope="col">Hours</th>
+                      <th scope="col">By Students</th>
+                      <th scope="col">Staff Involved</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Like a butterflies</td>
+                      <td>Boxing</td>
+                      <td>8:00 AM - 11:00 AM</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+const Appointments = ({ sidebarbtn_onClick }) => {
+  const [data, setData] = React.useState([]);
+
+  return (
+    <>
+      <p className="float-start d-lg-none d-md-block">
+        <Button variant="primary" size="sm" onClick={sidebarbtn_onClick}>
+          <i className="bi bi-chevron-right"></i>
+        </Button>
+      </p>
+      <Jumbotron header="Appointments">
+        Let people know when you are available
+      </Jumbotron>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header as="h2">Manage Appointments</Card.Header>
+            <Card.Body>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+type SidebarColBtnType = {
+  name: string;
+  iconClass: string;
+  value: string;
+  link: string;
+};
+const SidebarCol = () => {
+  const [radioValue, setRadioValue] = React.useState("1");
+
+  const radios: SidebarColBtnType[] = [
+    { name: "Dashboard", iconClass: "bi-columns-gap", value: "1", link: "/" },
+    {
+      name: "Analytics",
+      iconClass: "bi-clipboard",
+      value: "2",
+      link: "/appointments",
+    },
+    {
+      name: "Feedback",
+      iconClass: "bi-graph-up",
+      value: "3",
+      link: "/feedback",
+    },
+  ];
+
+  return (
+    <Col
+      sm={6}
+      lg={{ span: 3, order: "1" }}
+      className="sidebar-offcanvas"
+      id="sidebar"
+    >
+      <div>
+        <ButtonGroup className="d-grid gap-2">
+          {radios.map((radio, idx) => (
+            <SidebarColBtn
+              idx={idx}
+              name={radio.name}
+              iconClass={radio.iconClass}
+              value={radio.value}
+              radioValue={radioValue}
+              setRadioValue={setRadioValue}
+              link={radio.link}
+            />
+          ))}
+        </ButtonGroup>
+      </div>
+    </Col>
+  );
+};
+
+const SidebarColBtn = ({
+  idx,
+  name,
+  iconClass,
+  value,
+  radioValue,
+  setRadioValue,
+  link,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <ToggleButton
+      key={idx}
+      id={`radio-${idx}`}
+      type="radio"
+      variant={radioValue === value ? "primary" : "secondary"}
+      name="radio"
+      value={value}
+      checked={radioValue === value}
+      onChange={(e) => {
+        navigate(link);
+        setRadioValue(e.currentTarget.value);
+      }}
+      size="lg"
+      className="shadow-sm rounded-pill"
+      style={{ fontSize: "1.5rem" }}
+    >
+      <i className={`bi ${iconClass} mx-2`}></i>
+      {name}
+    </ToggleButton>
+  );
+};
+
+const App: React.FC = () => {
   const [isActive, setIsActive] = React.useState(false);
   const sidebarbtn_onClick = () => setIsActive(!isActive);
   const mainRowClassName = `row-offcanvas row-offcanvas-left ${
@@ -68,128 +260,43 @@ const App = () => {
   return (
     <BrowserRouter>
       <TopBar />
-      <Stack>
+      <Stack className="mt-4 px-2">
         <Container fluid>
           <Row className={mainRowClassName}>
-            <Col sm={12} md={{ span: 6, order: 3 }} id="ui-body">
-              <p className="float-start d-md-none d-sm-block">
-                <Button
-                  variant="primary"
-                  size="sm"
-                  onClick={sidebarbtn_onClick}
-                >
-                  <i className="bi bi-chevron-right"></i>
-                </Button>
-              </p>
-              <Jumbotron header="Dashboard">
-                Browse a summary of announcements and appointments relevant to
-                you.
-              </Jumbotron>
-              <Row>
-                <Col sm={12}>
-                  <Card>
-                    <Card.Header as="h2">Announcements</Card.Header>
-                    <Card.Body>
-                      <ListGroup>
-                        <ListGroupItem>Announcement 1</ListGroupItem>
-                        <ListGroupItem>Announcement 2</ListGroupItem>
-                      </ListGroup>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={12} md={{ order: 2 }}>
-                  <Card>
-                    <Card.Header as={"h2"}>My Appointments</Card.Header>
-                    <Card.Body>
-                      <div
-                        className="table-responsive"
-                        style={{
-                          height: "25rem",
-                          position: "relative",
-                        }}
-                      >
-                        <Table className="appointments-table">
-                          <thead>
-                            <tr className="text-uppercase">
-                              <th scope="col">Hours</th>
-                              <th scope="col">By Students</th>
-                              <th scope="col">Staff Involved</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>Like a butterflies</td>
-                              <td>Boxing</td>
-                              <td>9:00 AM - 11:00 AM</td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
+            <Col sm={12} lg={{ span: 6, order: 3 }} id="ui-body">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Dashboard sidebarbtn_onClick={sidebarbtn_onClick} />
+                  }
+                />
+                <Route
+                  path="/appointments"
+                  element={
+                    <Appointments sidebarbtn_onClick={sidebarbtn_onClick} />
+                  }
+                />
+              </Routes>
             </Col>
-            <Col
-              sm={6}
-              md={{ span: 3, order: "1" }}
-              className="sidebar-offcanvas"
-              id="sidebar"
-            >
-              <div className="list-group">
-                <a href="#" className="list-group-item active">
-                  I'm the Sidebar
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-              </div>
-            </Col>
+            <SidebarCol />
             <Col
               sm={12}
-              md={{ span: 3, order: "last" }}
+              lg={{ span: 3, order: "last" }}
               style={{ marginBottom: "3rem" }}
             >
-              <div className="list-group">
-                <a href="#" className="list-group-item active">
-                  I'm the Sidebar
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-                <a href="#" className="list-group-item">
-                  Link
-                </a>
-              </div>
+              <Card className="shadow-sm">
+                <Card.Header as={"h2"}>People Online</Card.Header>
+                <Card.Body>
+                  <ListGroup>
+                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </Container>
--- src/frontend-react/src/index.tsx
+++ src/frontend-react/src/index.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
