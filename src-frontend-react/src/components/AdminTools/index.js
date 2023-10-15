504171134fa9354ef01bc3b986f36d4da8348968
Christian Aranas <aranaschristianlouise@gmail.com>
2023-10-15 18:12:28 +0800

    Hide AdminTools UI when not an Admin
    AdminTools now don't render when the Cookies `usertype` does not equal to "Admin"

--- src-frontend-react/src/components/AdminTools/index.js
+++ src-frontend-react/src/components/AdminTools/index.js
@@ -3,42 +3,49 @@ import { Jumbotron } from "../Jumbotron";
 import { AnnouncementsForm } from "./AnnouncementsForm";
 import RegistrationForm from "./RegistrationForm";
 import UserApprovalComponent from "./UserApproval";
+import Cookies from "js-cookie";
 
 export const AdminTools = ({ sidebarbtn_onClick }) => {
+  const usertype = Cookies.get("usertype")
 
   return (
     <>
-      <p className="float-start d-lg-none d-md-block">
-        <Button variant="primary" size="sm" onClick={sidebarbtn_onClick}>
-          <i className="bi bi-chevron-right"></i>
-        </Button>
-      </p>
-      <Jumbotron header="Admin Tools">
-        Create and update database details
-      </Jumbotron>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Header as="h2">Users Management</Card.Header>
-            <Card.Body>
-              <RegistrationForm />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card className="shadow-sm">
-            <Card.Header as={"h2"}>Announcements Management</Card.Header>
-            <Card.Body>
-              <AnnouncementsForm />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <UserApprovalComponent />
-      </Row>
+      {usertype === "Admin" ?
+        <>
+          <p className="float-start d-lg-none d-md-block">
+            <Button variant="primary" size="sm" onClick={sidebarbtn_onClick}>
+              <i className="bi bi-chevron-right"></i>
+            </Button>
+          </p>
+          <Jumbotron header="Admin Tools">
+            Create and update database details
+          </Jumbotron>
+          <Row>
+            <Col>
+              <Card>
+                <Card.Header as="h2">Users Management</Card.Header>
+                <Card.Body>
+                  <RegistrationForm />
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Card className="shadow-sm">
+                <Card.Header as={"h2"}>Announcements Management</Card.Header>
+                <Card.Body>
+                  <AnnouncementsForm />
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <UserApprovalComponent />
+          </Row>
+        </>
+        : null
+      }
     </>
   );
 };
