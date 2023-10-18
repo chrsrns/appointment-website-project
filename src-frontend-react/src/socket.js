4bcdd2c46b27c4ccebc18e61af5e0fd4255cb81d
Christian Aranas <aranaschristianlouise@gmail.com>
2023-10-19 05:05:21 +0800

    Revert "Changed protocol for socket io"
    This reverts commit f1d40b567cd2ddc0d125069b9472e722f0b83846.

--- src-frontend-react/src/socket.js
+++ src-frontend-react/src/socket.js
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 const env = process.env.NODE_ENV;
 const url = env === 'development' ? "localhost:3000" : ""
 
-export const socket = io(url, { autoConnect: false, transports: ['websocket'] });
+export const socket = io(url, { autoConnect: false });
