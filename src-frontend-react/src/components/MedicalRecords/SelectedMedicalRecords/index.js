26a43a01df3e14013e69e45d4e1e680e76a65e70
Christian Aranas <aranaschristianlouise@gmail.com>
2023-11-03 20:06:55 +0800

    Fixed eslint issues

--- src-frontend-react/src/components/MedicalRecords/SelectedMedicalRecords/index.js
+++ src-frontend-react/src/components/MedicalRecords/SelectedMedicalRecords/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Form, Button, Stack, Card, Accordion } from 'react-bootstrap';
+import { Card, Accordion } from 'react-bootstrap';
 import Select from 'react-select'
 
 import LoadingOverlay from 'react-loading-overlay-ts';
