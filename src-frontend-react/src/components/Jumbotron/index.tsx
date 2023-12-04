e30395a94733caf4d582b440cf624c6bc319680b
Christian Aranas <aranaschristianlouise@gmail.com>
2023-12-04 19:53:35 +0800

    Added gradient animation on Jumbotron
    Added animation on App.css, declaring the properties.
    Added declaration of background-size. The size needs to exceed 100%, as the animation relies on the translation on the position of the background gradient.
    The background size and image gradient is set as important to prevent darkreader from changing it.
    Condition-based background image declaration is added to Jumbotron. It uses a reference callback as React do not support setting of !important through the normal React method of setting the styles.

--- src-frontend-react/src/components/Jumbotron/index.tsx
+++ src-frontend-react/src/components/Jumbotron/index.tsx
@@ -1,11 +1,28 @@
+import { useCookies } from "react-cookie";
+
 type JumbotronProps = {
   header: string;
   children: React.ReactNode;
 };
 
 export const Jumbotron: React.FC<JumbotronProps> = ({ header, children }) => {
+  const [cookies] = useCookies(["darkmode"]);
+  const primaryColor = cookies.darkmode ? "#0250c4" : "#cfe2ff";
+  const secondaryColor = cookies.darkmode ? "#6610f2" : "#dfcefb";
+
   return (
-    <div className="jumbotron rounded-3 shadow-sm bg-success-subtle">
+    <div
+      className="jumbotron jumbotron-anim rounded-3 shadow-sm"
+      ref={(node) => {
+        if (node) {
+          node.style.setProperty(
+            "background-image",
+            `linear-gradient(232deg, ${secondaryColor}, ${primaryColor})`,
+            "important",
+          );
+        }
+      }}
+    >
       <h1>{header}</h1>
       <p>{children}</p>
     </div>
