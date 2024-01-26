5bd315e0de69b4641e8943441aa3153e5542276d
Christian Aranas <aranaschristianlouise@gmail.com>
2024-01-26 23:42:51 +0800

    Replaced deprecated substr to substring

--- src-frontend-react/src/components/LoginFormModal/index.js
+++ src-frontend-react/src/components/LoginFormModal/index.js
@@ -139,7 +139,7 @@ export const LoginFormModal = ({ show, onHide }) => {
       >
         <Modal.Title>
           {tabKey.replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
           })}
         </Modal.Title>
       </Modal.Header>
