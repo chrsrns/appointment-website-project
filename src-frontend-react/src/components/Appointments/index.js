d4ad8ed35389b3ed8bfe8c4650366201aafb770d
Christian Aranas <aranaschristianlouise@gmail.com>
2024-01-30 17:19:05 +0800

    eslint fixes
    Removed unneeded Button import.

--- src-frontend-react/src/components/Appointments/index.js
+++ src-frontend-react/src/components/Appointments/index.js
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { Jumbotron } from "../Jumbotron";
 
 import { momentLocalizer } from "react-big-calendar";
