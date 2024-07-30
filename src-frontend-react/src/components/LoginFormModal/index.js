03f80f445265f2c331488ee1f7b72e5c6200ce68
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-07-30 21:18:05 +0800

    Added designs to `LoginFormModal`

--- src-frontend-react/src/components/LoginFormModal/index.js
+++ src-frontend-react/src/components/LoginFormModal/index.js
@@ -28,6 +28,56 @@ export const LoginFormModal = ({ show, onHide }) => {
       onHide={() => onHide()}
       size={tabKey === "register" ? "lg" : ""}
     >
+      <div className="position-relative">
+        <div
+          className="position-absolute"
+          style={{
+            zIndex: -1,
+            left: "-7.5em",
+            top: "-7.5em",
+            rotate: "-25deg",
+            opacity: 0.8,
+          }}
+        >
+          <svg
+            fill="currentColor"
+            stroke-width="0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            style={{ overflow: "visible", color: "currentcolor" }}
+            height="15em"
+            width="15em"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14.5 2H13V1h-1v1H4V1H3v1H1.5l-.5.5v12l.5.5h13l.5-.5v-12l-.5-.5zM14 14H2V5h12v9zm0-10H2V3h12v1zM4 8H3v1h1V8zm-1 2h1v1H3v-1zm1 2H3v1h1v-1zm2-4h1v1H6V8zm1 2H6v1h1v-1zm-1 2h1v1H6v-1zm1-6H6v1h1V6zm2 2h1v1H9V8zm1 2H9v1h1v-1zm-1 2h1v1H9v-1zm1-6H9v1h1V6zm2 2h1v1h-1V8zm1 2h-1v1h1v-1zm-1-4h1v1h-1V6z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <div
+          className="position-absolute"
+          style={{
+            zIndex: -1,
+            right: "-2em",
+            top: "-2em",
+            rotate: "-25deg",
+            opacity: 0.8,
+          }}
+        >
+          <svg
+            fill="currentColor"
+            stroke-width="0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            style={{ overflow: "visible", color: "currentcolor" }}
+            height="4em"
+            width="4em"
+          >
+            <path d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512z"></path>
+          </svg>
+        </div>
+      </div>
       <Modal.Header
         closeButton={tabKey !== "Data Privacy Acknowledge and Consent"}
         data-bs-theme={cookies.darkmode ? "dark" : "light"}
